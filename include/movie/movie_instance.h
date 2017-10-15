aa9c2a5cfcdd9fef7d7ce9065a0c8f824a8669b9
irov <irov13@mail.ru>
2017-10-15 18:06:49 +0300

    fix LICENSE

--- include/movie/movie_instance.h
+++ include/movie/movie_instance.h
@@ -1,13 +1,13 @@
 /******************************************************************************
 * libMOVIE Software License v1.0
 *
-* Copyright (c) 2016-2017, Levchenko Yuriy <irov13@mail.ru>
+* Copyright (c) 2016-2017, Yuriy Levchenko <irov13@mail.ru>
 * All rights reserved.
 *
 * You are granted a perpetual, non-exclusive, non-sublicensable, and
 * non-transferable license to use, install, execute, and perform the libMOVIE
 * software and derivative works solely for personal or internal
-* use. Without the written permission of Levchenko Yuriy, you may not (a) modify, translate,
+* use. Without the written permission of Yuriy Levchenko, you may not (a) modify, translate,
 * adapt, or develop new applications using the libMOVIE or otherwise
 * create derivative works or improvements of the libMOVIE or (b) remove,
 * delete, alter, or obscure any trademarks or any copyright, trademark, patent,
@@ -15,10 +15,10 @@
 * Software, including any copy thereof. Redistributions in binary or source
 * form must include this license and terms.
 *
-* THIS SOFTWARE IS PROVIDED BY LEVCHENKO YURIY "AS IS" AND ANY EXPRESS OR
+* THIS SOFTWARE IS PROVIDED BY YURIY LEVCHENKO "AS IS" AND ANY EXPRESS OR
 * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
-* EVENT SHALL LEVCHENKO YURIY BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* EVENT SHALL YURIY LEVCHENKO BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, BUSINESS INTERRUPTION,
 * OR LOSS OF USE, DATA, OR PROFITS) HOWEVER CAUSED AND ON ANY THEORY OF
