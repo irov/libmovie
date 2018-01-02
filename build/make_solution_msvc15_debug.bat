cc53f77d9fec8e2c2a2b68ae52667eac9c6ebc2d
irov <irov13@mail.ru>
2018-01-02 13:43:06 +0200

    fix build

---
+++ build/make_solution_msvc15_debug.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set "build_dir=build"
+set "configuration=Debug"
+set "SOLUTION_DIR=solutions\libmovie_msvc15_debug"
+
+@pushd ..
+@mkdir %SOLUTION_DIR%
+@pushd %SOLUTION_DIR%
+CMake -G "Visual Studio 15 2017" "%CD%\..\.." -DCMAKE_CONFIGURATION_TYPES:STRING='Debug' -DCMAKE_BUILD_TYPE:STRING='Debug' -DLIBMOVIE_EXAMPLES_BUILD:BOOL=TRUE -DLIBMOVIE_COCOS2DX_BUILD:BOOL=TRUE
+@popd
+@popd
+
+@echo on
+@pause
--- build/make_solution_msvc15_release.bat
+++ build/make_solution_msvc15_release.bat
@@ -9,6 +9,7 @@ set "SOLUTION_DIR=solutions\libmovie_msvc15_release"
 @pushd %SOLUTION_DIR%
 CMake -G "Visual Studio 15 2017" "%CD%\..\.." -DCMAKE_CONFIGURATION_TYPES:STRING='Release' -DCMAKE_BUILD_TYPE:STRING='Release' -DLIBMOVIE_EXAMPLES_BUILD:BOOL=TRUE -DLIBMOVIE_COCOS2DX_BUILD:BOOL=FALSE
 @popd
+@popd
 
 @echo on
 @pause
