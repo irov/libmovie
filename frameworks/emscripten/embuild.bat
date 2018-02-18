1a728fab88aaceaf1a330393d0ef3b8d81b8600c
irov <irov13@mail.ru>
2018-02-18 20:17:45 +0200

    update frameworks emscripten

--- frameworks/emscripten/embuild.bat
+++ frameworks/emscripten/embuild.bat
@@ -1,2 +1,7 @@
-cmake.exe -G "Unix Makefiles" . -DEMSCRIPTEN_DIR=%EMSCRIPTEN% -DCMAKE_TOOLCHAIN_FILE=%EMSCRIPTEN%\cmake\Modules\Platform\Emscripten.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=mingw32-make
+mkdir build
+pushd build
+
+cmake.exe -G "Unix Makefiles" .. -DEMSCRIPTEN_DIR=%EMSCRIPTEN% -DCMAKE_TOOLCHAIN_FILE=%EMSCRIPTEN%\cmake\Modules\Platform\Emscripten.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=mingw32-make
 mingw32-make
+
+@popd
