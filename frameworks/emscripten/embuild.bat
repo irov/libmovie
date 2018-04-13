c2b831c0ec30e2994ba45179459d2bef3a0ba6ef
irov <irov13@mail.ru>
2018-04-13 17:07:28 +0300

    fix embuild.bat remove glClearColor

--- frameworks/emscripten/embuild.bat
+++ frameworks/emscripten/embuild.bat
@@ -3,7 +3,7 @@ pushd build
 
 del "../bin/em_movie.js"
 
-cmake.exe -G "Unix Makefiles" .. -DEMSCRIPTEN_DIR=%EMSCRIPTEN% -DCMAKE_TOOLCHAIN_FILE=%EMSCRIPTEN%\cmake\Modules\Platform\Emscripten.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=mingw32-make
+cmake.exe -G "Unix Makefiles" .. -DEMSCRIPTEN_DIR="%EMSCRIPTEN%" -DCMAKE_TOOLCHAIN_FILE="%EMSCRIPTEN%\cmake\Modules\Platform\Emscripten.cmake" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=mingw32-make
 mingw32-make
 
 @popd
