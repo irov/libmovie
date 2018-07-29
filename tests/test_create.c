6fcdeedd97712f1e4e2cd5594154b7f1792a586e
yuriy.levchenko <irov13@mail.ru>
2018-07-29 19:03:38 +0300

    wip tests

--- tests/test_create.c
+++ tests/test_create.c
@@ -4,16 +4,37 @@
 
 static const ae_char_t * ex_example_license_hash = "52ad6f051099762d0a0787b4eb2d07c8a0ee4491";
 
+AE_CALLBACK ae_voidptr_t stdlib_movie_alloc( ae_voidptr_t _data, ae_size_t _size ) {
+    AE_UNUSED( _data );
+    return malloc( _size );
+}
+
+AE_CALLBACK ae_voidptr_t stdlib_movie_alloc_n( ae_voidptr_t _data, ae_size_t _size, ae_size_t _count ) {
+    AE_UNUSED( _data );
+    ae_size_t total = _size * _count;
+    return malloc( total );
+}
+
+AE_CALLBACK ae_void_t stdlib_movie_free( ae_voidptr_t _data, ae_constvoidptr_t _ptr ) {
+    AE_UNUSED( _data );
+    free( (ae_voidptr_t)_ptr );
+}
+
+AE_CALLBACK ae_void_t stdlib_movie_free_n( ae_voidptr_t _data, ae_constvoidptr_t _ptr ) {
+    AE_UNUSED( _data );
+    free( (ae_voidptr_t)_ptr );
+}
+
 int main( int argc, char *argv[] )
 {
     AE_UNUSED( argc );
     AE_UNUSED( argv );
 
     const aeMovieInstance * instance = ae_create_movie_instance( ex_example_license_hash
-        , (ae_movie_alloc_t)AE_NULL
-        , (ae_movie_alloc_n_t)AE_NULL
-        , (ae_movie_free_t)AE_NULL
-        , (ae_movie_free_n_t)AE_NULL
+        , &stdlib_movie_alloc
+        , &stdlib_movie_alloc_n
+        , &stdlib_movie_free
+        , &stdlib_movie_free_n
         , (ae_movie_strncmp_t)AE_NULL
         , (ae_movie_logger_t)AE_NULL
         , AE_NULL );
