1469b20b779a648c4ba094c480d0d792242cdf34
irov <irov13@mail.ru>
2019-02-08 01:22:50 +0200

    update LICENSE

--- src/movie_transformation.c
+++ src/movie_transformation.c
@@ -1,7 +1,7 @@
 /******************************************************************************
 * libMOVIE Software License v1.0
 *
-* Copyright (c) 2016-2018, Yuriy Levchenko <irov13@mail.ru>
+* Copyright (c) 2016-2019, Yuriy Levchenko <irov13@mail.ru>
 * All rights reserved.
 *
 * You are granted a perpetual, non-exclusive, non-sublicensable, and
@@ -201,7 +201,9 @@ AE_INTERNAL ae_float_t __get_movie_layer_transformation_property_fixed( ae_const
         }break;
     }
 
+#ifdef AE_MOVIE_DEBUG
     __movie_break_point();
+#endif
 
     return 0.f;
 }
